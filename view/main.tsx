2f4e8ba4b5a1c95a4edbb873a65845d631074c68
Andres Mestra <andres-mestra@users.noreply.github.com>
2022-07-08 11:22:21 -0500

    refactor: define root react

--- view/main.tsx
+++ view/main.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { ElectronApp } from './ElectronApp'
 
-ReactDOM.render(<ElectronApp />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<ElectronApp />)
